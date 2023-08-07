2d5d1853b5d33f729436f85bd00259c30a044ff5
varjolintu <sami.vanttinen@ahmala.org>
2023-08-07 10:19:02 +0300

    Add option to disable database lock when switching user on macOS

--- src/gui/DatabaseTabWidget.h
+++ src/gui/DatabaseTabWidget.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 KeePassXC Team <team@keepassxc.org>
+ * Copyright (C) 2023 KeePassXC Team <team@keepassxc.org>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -75,6 +75,7 @@ public slots:
 
     bool lockDatabases();
     void lockDatabasesDelayed();
+    void lockDatabasesOnUserSwitch();
     void closeDatabaseFromSender();
     void unlockDatabaseInDialog(DatabaseWidget* dbWidget, DatabaseOpenDialog::Intent intent);
     void unlockDatabaseInDialog(DatabaseWidget* dbWidget, DatabaseOpenDialog::Intent intent, const QString& filePath);
